510ff3a6b3d6201f5ae2cd5d7a189799f5f8fa0d
Đỗ Đình Nam <namdoel1412@gmail.com>
2023-04-07 15:54:16 +0700

    ll

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from fastapi import FastAPI,  Query
 import uvicorn
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/items")
