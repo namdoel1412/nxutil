4dc4173c9065a277cb6d0c5deb24f98d2a08ccea
Đỗ Đình Nam <namdoel1412@gmail.com>
2023-04-12 16:20:53 +0700

    update

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def create_item(item: Item=Body(...)):
     f = open("naxsi.log","w+")
     for i in item.logs:
         f.write(f"{i}\n")
-    res = os.popen('python nx_util.py -l $(pwd)/naxsi.log -o -p 1 >> /usr/share/nginx/whitelist/testnaxsi.wl.rules').read()
+    res = os.popen('python nx_util.py -l /usr/share/nginx/whitelist/testnaxsi.vn.log -o -p 1 >> /usr/share/nginx/whitelist/testnaxsi.wl.rules').read()
     print(res)
     return res
 
